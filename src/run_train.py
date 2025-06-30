cce145a1646c8b4f7c3eb1da4e818521af1773d1
lucashlaing <mhlaing@ucsd.edu>
2025-06-29 21:10:13 -0700

    fixed s3 upload to proper s3 folder

--- src/run_train.py
+++ src/run_train.py
@@ -130,8 +130,9 @@ def run_train(cfg, model_class, dataset_class, trainer_class):
             ratio = (trainer.train_step - cfg.time_warm) / total_steps
             timer.estimate_time("time estimate", ratio)
 
-    upload_s3_path(cfg.plot_dir)
-    upload_s3_path(cfg.dump_dir)
+    s3_base_path = f"TGLF_MOE/{time_stamp}/"
+    upload_s3_path(s3_base_path, cfg.plot_dir)
+    upload_s3_path(s3_base_path, cfg.dump_dir)
     if cfg.board:
         wandb.finish()
 
